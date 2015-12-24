4205b1a6aaaea204602b5711cee1b1084d0eaef9
constantince <alberteinstein007@126.com>
2015-12-24 18:08:34 +0800

    submit a version base file

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,30 @@
 var express = require('express');
 var router = express.Router();
+var fs = require('fs');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/page', function(req, res, next) {
+	//输出模板 jade
+  	//res.render('index', { title: 'Express' });
+  	//输出文件静态html
+ 	fs.readFile('./public/page_main.html', 'utf-8', function(err, data) {//读取内容
+        if(err) throw err;
+        res.writeHead(200, {"Content-Type":"text/html"});
+        res.write(data);
+        res.end();
+    });
+});
+router.get('/web', function(req, res, next) {
+	//输出模板 jade
+  	//res.render('index', { title: 'Express' });
+  	//输出文件静态html
+ 	// fs.readFile('./public/page_main.html', 'utf-8', function(err, data) {//读取内容
+  //       if(err) throw err;
+  //       res.writeHead(200, {"Content-Type":"text/html"});
+  //       res.write(data);
+  //       res.end();
+  //   });
+  res.json({json: 'hello'});
 });
 
 module.exports = router;
